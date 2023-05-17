21496d5b5fcc30eca51d264a71539b2c4ca22cff
Jarrett Bierman <jarrettbierman@gmail.com>
2023-05-17 01:32:35 +0000

    audio stuff

--- story.py
+++ story.py
@@ -1,10 +1,10 @@
 from decouple import config
 from PIL import Image
+from gtts import gTTS
 from datetime import datetime
 import openai
 import requests
 import os
-import re
 
 openai.api_key = config('GPT_KEY')
 openai.organization = config("ORG")
@@ -12,8 +12,10 @@ openai.organization = config("ORG")
 
 
 def main():
-    generate_audio_visuals("Generate a story about four brothers fighting Spongebob Squarepant. Set up a exposition, conflict, and resolution. Separate the story into atleast 5 paragraphs. Output the story as 2 separate lists. One list, named \"TEXT\", contains each paragraph of the story in order. The second list, named \"IMAGES\", contains a generated image description based off each element in \"TEXT\". Do not number the elements of the list.")
-    
+    # generate_audio_visuals("Generate a story about four brothers fighting Spongebob Squarepant. Set up a exposition, conflict, and resolution. Separate the story into atleast 5 paragraphs. Output the story as 2 separate lists. One list, named \"TEXT\", contains each paragraph of the story in order. The second list, named \"IMAGES\", contains a generated image description based off each element in \"TEXT\". Do not number the elements of the list.")
+    tts = gTTS("hello world")
+    tts.save("hello.mp3")
+    os.system("mpg321 hello.mp3")
 
 def generate_audio_visuals(prompt):
     # set up folder
